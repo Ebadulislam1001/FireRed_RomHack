801757bc3c8c504195233825f5c0acf12500f71d
Ebadulislam <ebadulislamfarooqi@gmail.com>
2024-10-29 16:07:28 +0530

    Added BulletPunch, FieryShots, SpeedStrike

--- docs/moves_file_handler.cpp
+++ docs/moves_file_handler.cpp
@@ -33,14 +33,6 @@ int main()
     readMoveList(moveList);
     updateNames(moveList);
     printCSV(moveList);
-    stable_sort(moveList.begin(), moveList.end(), [](Move a, Move b)
-                { return a.pp > b.pp; });
-    stable_sort(moveList.begin(), moveList.end(), [](Move a, Move b)
-                { return a.accuracy > b.accuracy; });
-    stable_sort(moveList.begin(), moveList.end(), [](Move a, Move b)
-                { return a.basePower > b.basePower; });
-    stable_sort(moveList.begin(), moveList.end(), [](Move a, Move b)
-                { return a.type < b.type; });
     printMoveList(moveList); // use new names
 }
 
@@ -176,27 +168,58 @@ void printMoveList(vector<Move> &moveList)
         return;
     }
 
-    string lineSeparator = "+-------+----------------+----------+------------+----------+-----------------+----------+--------------------------+----------+----------+";
+    // Apply custom sorting on the moveList
+    stable_sort(moveList.begin(), moveList.end(), [](Move a, Move b)
+                { return a.priority > b.priority; });
+    stable_sort(moveList.begin(), moveList.end(), [](Move a, Move b)
+                { return a.pp > b.pp; });
+    stable_sort(moveList.begin(), moveList.end(), [](Move a, Move b)
+                { return a.accuracy > b.accuracy; });
+    stable_sort(moveList.begin(), moveList.end(), [](Move a, Move b)
+                { return a.effectChance > b.effectChance; });
+    stable_sort(moveList.begin(), moveList.end(), [](Move a, Move b)
+                { return a.target > b.target; });
+    stable_sort(moveList.begin(), moveList.end(), [](Move a, Move b)
+                { return a.effect < b.effect; });
+    stable_sort(moveList.begin(), moveList.end(), [](Move a, Move b)
+                { return a.basePower > b.basePower; });
+    stable_sort(moveList.begin(), moveList.end(), [](Move a, Move b)
+                { return a.type < b.type; });
+
+    // Print the sorted moveList
+    string lineSeparator = "+-------+----------------+----------+------------+----------+--------------------------+----------+-----------------+----------+----------+";
     fprintf(writePointer, "%s\n", lineSeparator.c_str());
-    fprintf(writePointer, "| INDEX |  NAME          | TYPE     | BASE POWER | ACCURACY |     TARGET      |    PP    | ADDITIONAL EFFECT NAME   | EFFECT % | PRIORITY |\n");
+    fprintf(writePointer, "| INDEX |  NAME          | TYPE     | BASE POWER | ACCURACY | ADDITIONAL EFFECT NAME   | EFFECT % |     TARGET      |    PP    | PRIORITY |\n");
     fprintf(writePointer, "%s\n", lineSeparator.c_str());
 
     for (int i = 0, j = 0; i < moveList.size(); i++)
     {
         Move thisMove = moveList[i];
-
-        fprintf(writePointer, "| %4d  ", thisMove.index);
-        fprintf(writePointer, "| %-14s ", thisMove.name.c_str());
-        fprintf(writePointer, "| %-8s ", thisMove.type.c_str());
-        fprintf(writePointer, "| %10d ", thisMove.basePower);
-        fprintf(writePointer, "| %8d ", thisMove.accuracy);
-        fprintf(writePointer, "| %-15s ", thisMove.target.c_str());
-        fprintf(writePointer, "| %8d ", thisMove.pp);
-        fprintf(writePointer, "| %-24s ", thisMove.effect.c_str());
-        fprintf(writePointer, "| %8d ", thisMove.effectChance);
-        fprintf(writePointer, "| %+8d ", thisMove.priority);
-        fprintf(writePointer, "|\n%s\n", lineSeparator.c_str());
-        // printf("successfully wrote %dth Move info\n", i + 1);
+        // bool condition = true;
+        // bool condition = thisMove.type == "BUG";
+        // bool condition = thisMove.basePower >= 100;
+        // bool condition = thisMove.accuracy < 100;
+        // bool condition = thisMove.target == "USER";
+        // bool condition = thisMove.effectChance >= 50;
+        bool condition = thisMove.effect == "MULTI_HIT";
+        // bool condition = thisMove.priority < 0;
+        // bool condition = true;
+        if (condition)
+        {
+            // fprintf(writePointer, "| %4d  ", thisMove.index);
+            fprintf(writePointer, "| %4d  ", ++j);
+            fprintf(writePointer, "| %-14s ", thisMove.name.c_str());
+            fprintf(writePointer, "| %-8s ", thisMove.type.c_str());
+            fprintf(writePointer, "| %10d ", thisMove.basePower);
+            fprintf(writePointer, "| %8d ", thisMove.accuracy);
+            fprintf(writePointer, "| %-24s ", thisMove.effect.c_str());
+            fprintf(writePointer, "| %8d ", thisMove.effectChance);
+            fprintf(writePointer, "| %-15s ", thisMove.target.c_str());
+            fprintf(writePointer, "| %8d ", thisMove.pp);
+            fprintf(writePointer, "| %+8d ", thisMove.priority);
+            fprintf(writePointer, "|\n%s\n", lineSeparator.c_str());
+            // printf("successfully wrote %dth Move info\n", i + 1);}
+        }
     }
 
     fclose(writePointer);
